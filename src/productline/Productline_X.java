bd378ce3a3582f64f3aefe9444fca00c92097ebc
garyopen1876 <garyopen1876@gmail.com>
2019-01-08 20:20:23 +0800

    1/8 final 2.0

--- src/productline/Productline_X.java
+++ src/productline/Productline_X.java
@@ -13,8 +13,8 @@ class Productline_X extends Productline{
 		Component part1 = createUpper();
 		Component part2 = createOutsole();
 		Component part3 = createLace();
-		Component part4 = createOutsole();
-		Component part5 = createLace();
+		Component part4 = createBox();
+		Component part5 = createHead();
 		worklist[0] = part1;
 		worklist[1] = part2;
 		worklist[2] = part3;
@@ -36,6 +36,16 @@ class Productline_X extends Productline{
 		//製作鞋帶
 		return new Lace();
 	}
+	
+	public Component createBox() {
+    //製作鞋合
+    return new Box();
+  }
+
+  public Component createHead() {
+    //製作鞋頭
+    return new Head();
+  }
 }
 
 class Upper extends Component {
@@ -60,4 +70,20 @@ class Lace extends Component {
 		condition = "Carefulness";
 		position = "make Lace";
 	}
+}
+
+class Box extends Component {
+  //鞋盒實體
+  public Box() {
+    condition = "Carefulness";
+    position = "make Box";
+  }
+}
+
+class Head extends Component {
+  //鞋頭實體
+  public Head() {
+    condition = "Job score";
+    position = "make Head";
+  }
 }
